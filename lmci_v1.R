e0fbece98897b6b00392a5124de67bb6bb8f283b
gabrimarconi <gabriele.marconi@ext.ec.europa.eu>
2021-03-17 15:57:40 +0000

    commit 16:57

--- lmci_v1.R
+++ lmci_v1.R
@@ -172,10 +172,17 @@ lmcirun <- function(x){
   sumstats_by_company$culn_undup_n <- sumstats_by_company$ln_undup_n^3
   sumstats_by_company$quln_undup_n <- sumstats_by_company$ln_undup_n^4
   sumstats_by_company$ln_n <- log(sumstats_by_company$tot_n)
+  sumstats_by_company$ln_province <- log(sumstats_by_company$idprovince)
+  sumstats_by_company$ln_sector <- log(sumstats_by_company$idsector)
+  sumstats_by_company$ln_undup_prov <- sumstats_by_company$ln_province * sumstats_by_company$ln_undup_n
+  
   
   testflag1 <- automflag(xvar2="sqln_undup_n", xvar3="culn_undup_n", xvar4="quln_undup_n")
   testflag2 <- automflag(yvar="ln_n", xvar1="ln_undup_n", xvar2="sqln_undup_n", flag_above=FALSE, flag_below=TRUE)
+  testflag3 <- automflag(yvar="ln_sector", xvar1="ln_prov", xvar2="ln_undup_n", xvar3="ln_undup_prov", flag_above=TRUE, flag_below=FALSE)
   automflag_output <- automflag_combine(automflag1= testflag1, automflag2= testflag2 )
+  automflag_output <- automflag_combine(automflag1= automflag_output, automflag2= testflag3 )
+  
   
   # automflag_output <- automflag(xvar2="sqln_undup_n", xvar3="culn_undup_n", xvar4="quln_undup_n")
   # comboflag <- as.character(automflag_output[[4]])
