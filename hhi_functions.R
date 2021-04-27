94423b95dca5c4da347e7372c255ee30af74234b
aschand <ae.ascheri@gmail.com>
2021-04-27 14:44:57 +0000

    Corrected mistake on createfua () for HU. Removed whitespaces with str_trim

--- hhi_functions.R
+++ hhi_functions.R
@@ -85,7 +85,7 @@ createfua <- function(countrycode){
     }  
     setnames(DT,gsub("\\s","_",colnames(DT)))
     setnames(DT,gsub("\\(.*\\)","",colnames(DT)))
-    
+    DT$FUA_ID <- str_trim(DT$FUA_ID, side = "right")
     # DT<-DT[,which(unlist(lapply(DT, function(x)!all(is.na(x))))),with=F]
     # message("DT-",countrycode,"\n")
     #finding which LAUs belong to the same NUTS and FUAs
